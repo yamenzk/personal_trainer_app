6550943f76be7a83e3c12713d0a8c9d8501d273c
yamenzk <98694759+yamenzk@users.noreply.github.com>
2024-11-23 16:29:09 +0400

    Reset

--- dashboard/src/utils/frappe.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/frappe.ts
-export const getSiteName = () => {
-    // @ts-ignore
-    if (window.frappe?.boot?.versions?.frappe && (window.frappe.boot.versions.frappe.startsWith('15') || window.frappe.boot.versions.frappe.startsWith('16'))) {
-      // @ts-ignore
-      return window.frappe?.boot?.sitename ?? import.meta.env.VITE_SITE_NAME
-    }
-    return import.meta.env.VITE_SITE_NAME
-  }
